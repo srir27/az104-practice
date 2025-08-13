a0931cecdc59b3d4d4b6d098a1fe991b42bf086b
sribalajirajarathinam <sribalaji7708@gmail.com>
2025-08-13 15:01:04 +0530

    modify main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ terraform {
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
-  subscription_id = "9a11755d-a926-4ef7-b329-2f1a3dc2e9c5"
+  
 }
 
 resource "azurerm_resource_group" "rg" {
